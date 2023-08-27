885a68dffcd63ce2944463df389f9b7c9dbdb774
Yazan <yazan5sulaiman@gmail.com>
2023-08-27 08:04:55 -0700

    implement add Flight

--- FlightBookingSystem.java
+++ FlightBookingSystem.java
@@ -1,7 +1,7 @@
 public class FlightBookingSystem{
     private int count=0;
     public Flight[] flights = new Flight[100];
-
+    public Ticket[] tickets = new Ticket[100];
     public void addFlight(Flight flight){
         flights[count] = flight;
         count++;
@@ -11,7 +11,9 @@ public class FlightBookingSystem{
     public static void Menu(){
         System.out.println();
         System.out.println("1. Add New Flight\n"+ "2. Book Ticket\n"+ "3. Update Ticket\n"+ "4. Remove Ticket\n" + "5. Print Booked Passengers\n"+ "6. Display Available Flights\n" +"7. Exit");
-    
     }
     
+    public void printAll(Flight flight,Ticket ticket){
+        System.out.println(flight,ticket);
+    }
 }
--- Ticket.java
+++ Ticket.java
@@ -7,6 +7,10 @@ public class Ticket{
         this.custmorName = custmorName;
         this.flight = flight;
     }
-
-
+    public void updateCustomerName(String custmorName){
+        this.custmorName = custmorName;
+    }
+    public String toString(){
+        return  custmorName; 
+    }
 }
